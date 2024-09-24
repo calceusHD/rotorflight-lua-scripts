5e9ac05cf115395ac9c6ab2ea633cfe35ff3b69b
calceushd <calceushd@gmail.com>
2024-09-24 16:13:39 +0200

    add option to reset persistent stats

--- src/SCRIPTS/RF2/PAGES/persistent_stats.lua
+++ src/SCRIPTS/RF2/PAGES/persistent_stats.lua
@@ -15,14 +15,17 @@ local function quot(a, b)
     return (a - (a % b)) / b
 end
 
+
+
 labels[#labels + 1] = { t = "Stats",                 x = x,          y = inc.y(lineSpacing) }
 fields[#fields + 1] = { t = "Total flight count",    x = x + indent, y = inc.y(lineSpacing), readOnly = true, sp = x + sp, min = 0, max = 4294967296, vals = { 1, 2, 3 ,4 } }
 fields[#fields + 1] = { t = "Total flight time",     x = x + indent, y = inc.y(lineSpacing), readOnly = true, sp = x + sp, min = 0, max = 4294967296, vals = { 5, 6, 7, 8 } }
 fields[#fields + 1] = { t = "Total flight distance", x = x + indent, y = inc.y(lineSpacing), readOnly = true, sp = x + sp, min = 0, max = 4294967296, vals = { 9, 10, 11, 12 } }
 
+fields[#fields + 1] = { t = "[Reset]",               x = x + indent, y = inc.y(lineSpacing) }
+
 return {
     read        = 152, -- MSP_PERSISTENT_STATS
-    --write       = , -- not writeable yet?
     eepromWrite = false,
     reboot      = false,
     readOnly    = true,
@@ -37,6 +40,23 @@ return {
         local time_min = math.fmod(time_raw, 60)
         local time_h = quot(time_raw, 60)
         self.fields[2].value = string.format("%02d h %02d min %02d s", time_h, time_min, time_s)
+
+        self.fields[4].preEdit = self.resetPersistentStats
+        self.isReady = true
     end,
-    simulatorResponse = { 0, 0, 0, 0, 0, 2, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    resetPersistentStats = function(field, self)
+        local message = {
+            command = 153, -- MSP_SET_PERSISTENT_STATS
+            payload = {},
+            processReply = function(self, buf)
+                rf2.readPage()
+            end,
+            simulatorResponse = { }
+        }
+        rf2.mspHelper.writeU32(message.payload, 0) --flight count
+        rf2.mspHelper.writeU32(message.payload, 0) --flight time
+        rf2.mspHelper.writeU32(message.payload, 0) --flight distance
+        rf2.mspQueue:add(message)
+    end,
+    simulatorResponse = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 }
